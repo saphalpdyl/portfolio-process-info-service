7bd270067348599d74c4a96730368c20fd74bda8
saphalpdyl <saphalpdyl@gmail.com>
2024-11-04 23:13:16 -0600

    add: created a Configuration serializable class

---
+++ lib/Configuration.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace _02_PingProcessInformationToWebsiteService.lib
+{
+    public struct Configuration
+    {
+        [JsonPropertyName("target_url")]
+        public string TargetURL { get; set; }
+
+        [JsonPropertyName("authorization_secret")]
+        public string AuthorizationSecret {  get; set; }
+
+        [JsonPropertyName("targets")]
+        public ProcessTarget[] ProcessTargets { get; set; }
+    }; 
+}
---
+++ lib/ProcessTarget.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace _02_PingProcessInformationToWebsiteService.lib
+{
+
+    public class ProcessTarget
+    {
+        public static readonly ProcessTarget[] DEFAULT_TARGETS = {
+            new ProcessTarget { ApplicationName = "Android Studio", ProcessName = "studio64" },
+            new ProcessTarget { ApplicationName = "Visual Studio Code", ProcessName = "Code" },
+            new ProcessTarget { ApplicationName = "Visual Studio IDE", ProcessName = "devenv" },
+            new ProcessTarget { ApplicationName = "Postman", ProcessName = "Postman" },
+            new ProcessTarget { ApplicationName = "Obsidian", ProcessName = "obsidian" },
+            new ProcessTarget { ApplicationName = "Chrome", ProcessName = "chrome" }
+        };
+
+        [JsonPropertyName("app_name")]
+        public string ApplicationName {  get; set; }
+
+        [JsonPropertyName("process_name")]
+        public string ProcessName {  get; set; }
+    }
+}
