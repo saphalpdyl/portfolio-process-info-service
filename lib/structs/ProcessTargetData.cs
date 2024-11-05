f9229a03aa0324cc9b6bceaf101d7f3497b58d6b
saphalpdyl <saphalpdyl@gmail.com>
2024-11-05 00:12:48 -0600

    add: created a new ProcessTargetData structure that will contain the processed data

---
+++ lib/structs/ProcessTargetData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace _02_PingProcessInformationToWebsiteService.lib.structs
+{
+    internal struct ProcessTargetData
+    {
+
+        [JsonPropertyName("appName")]
+        public string appName {  get; set; }
+
+        [JsonPropertyName("isRunning")]
+        public bool isRunning {  get; set; }
+
+    }
+}
