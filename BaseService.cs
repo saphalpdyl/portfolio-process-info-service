30cb6f1a8542b2545d603dcccd6c51358676a036
saphalpdyl <saphalpdyl@gmail.com>
2024-11-04 22:44:56 -0600

    Add project files.

---
+++ BaseService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_PingProcessInformationToWebsiteService
+{
+    public partial class BaseService : ServiceBase
+    {
+        public BaseService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
